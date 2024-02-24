6396163908c778bd9926f6ac8f722e270d39d046
motionbit95 <motionbit.dev@gmail.com>
2024-02-24 12:59:09 +0900

    포트번호 변경

--- client/src/setupProxy.js
+++ client/src/setupProxy.js
@@ -1,5 +1,9 @@
 const proxy = require("http-proxy-middleware");
 
 module.exports = function (app) {
-  app.use(proxy("/api", { target: "http://localhost:8081/" }));
+  app.use(
+    proxy("/api", {
+      target: "https://port-0-chat-bot-17xco2nlszge3vt.sel5.cloudtype.app/",
+    })
+  );
 };
--- index.js
+++ index.js
@@ -25,7 +25,7 @@ if (process.env.NODE_ENV === "production") {
   });
 }
 
-const port = process.env.PORT || 8081;
+const port = process.env.PORT || 5000;
 
 app.listen(port, () => {
   console.log(`Server Running at ${port}`);
