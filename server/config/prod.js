34a774fa74ac7f9edf4867b9c425dc256d92969d
motionbit95 <motionbit.dev@gmail.com>
2024-02-24 12:54:10 +0900

    temp

--- server/config/prod.js
+++ server/config/prod.js
@@ -1,8 +1,16 @@
 module.exports = {
-  googleProjectID: process.env.GOOGLE_PROJECT_ID,
-  dialogFlowSessionID: process.env.DIALOGFLOW_SESSION_ID,
-  dialogFlowSessionLanguageCode: process.env.DIALOGFLOW_LANGUAGE_CODE,
-  googleClientEmail: process.env.GOOGLE_CLIENT_EMAIL,
-  googlePrivateKey: JSON.parse(process.env.GOOGLE_PRIVATE_KEY),
-  mongoURI: process.env.MONGO_URI,
+  googleProjectID: "motionbit-chatbot",
+  dialogFlowSessionID: "bot-session",
+  dialogFlowSessionLanguageCode: "en-US",
+  googleClientEmail:
+    "motionbit-chatbot@motionbit-chatbot.iam.gserviceaccount.com",
+  googlePrivateKey:
+    "-----BEGIN PRIVATE KEY-----\nMIIEvAIBADANBgkqhkiG9w0BAQEFAASCBKYwggSiAgEAAoIBAQCV448685D4/VT4\nX/+OLaH1yOCR+Cwg+Ae0WGlHC8xuRVJ80cNM40KQb8ZJShwWRP2CKeeFodxD3xXr\neoeaCbNStW/RWGR+5rr+pmC0zUSCn99ZqjQSe/mHO2gSyzJl4Dgh45qV/6xlDOPw\nZqCEgbbqAhcjwkTX/DI6+UyaCz0A7pLfvxvQ1aoJ2kesQLxfF0S7KZTmJakWblDT\nuKP1/eU4PAlsM/OfQ+6QFaGyAPz9ck8mAzH46TU29vSI/5Ar/FaNmO9ZimkMfAGa\nffPs/nhpT0S0GoGfh7FYtcDBb6qlXNMtQ95ho6u8t5DqoKHFzn/hWAcfDDPhfOXh\nlabhEQbrAgMBAAECggEADKfVmJ8nHvbY3U3RuJH+yk9nMrv3EeOPVvleDjsfTDI7\nOEHH0S6hGKvvXJHO1EyH8R0TqNNVUz+7TJIgb++gvfuG0PC7L4HQyR6ILMvmHHde\nNuZpliP0mif4bhnNDBAF/UqH62FWj4OqnUo1BGslZBXAGWtWP7xSC2uwPZN0V6cr\n/uvtGzmjUmf6iB4vq5qwuGgMmhHpiqV30RTD3vgqZAQ+fMYWD9Jw52mZHrd4zLLM\nUnlQxtNdEHSGy8CFFvyYKHBnFQJuqriBXgABiAOW2zExkhw4Oh+yHJp5bwe++ewo\ni2HmwMH0Kh9uMsDEG4JFlNW0Z+eqQwErJ4wZaqVmjQKBgQDTpG4M5Z86FeFYKcSA\n0eEU91UXvnuloqy1RCh8JwXMAz7ceGHTbMPHh4e1lGGlaziaP9D8t9dBCjP2H0Na\nmXh/SQSpLuktuIHnjh508EHRhkT1QM2u0PpcBoK26i2Pp+PnHKlsXj8kyMB5Iu+b\nbMh7LgJtC61R4UCLTCeSNxBuNwKBgQC1TceJycCxsQ+tUzeEis/5ocpnLJct07To\n9SeCYcMWNu0BmkA9q+V1nnjBVJRUoCVxM++Tpg6mqyBvTn6R+9Nic1lVTH6AsX2x\n1tQl/4fBYggfsTXoR8oiz3o2n7XbWL6M6/cJLCQzfWMUoCktkQ3s3quSXiEi4LvB\nsghHeS3y7QKBgGnpxr0jiHyMCJqTRGhnH+nbILViOQSaFS4dbtk6uhbvpNy2TdZ9\nfrhypcKEAB3FO61NtGtUIR/GWFyaJp3HYdrpErEL7dtYk4HqyGki9v1A4W+Ov5/1\nquS0yWN6a9SRY2b8G/lEbPsej9vPVPOGDApA6yvhBRqNzDwpnz/32HwfAoGAXuLN\nCmzff1+2X7TMun5XqEMnheXNXTuOU9lyCweVLTwjwBH1ixE6rtr4Qd49HykNT3ip\n6vEee9XVzMsc7NQJ5l2OJFk7X6kEqp1PPG/GocvnioaZOu7PeHZzdMETUS/Z0jDw\ngglrDNOTlkjYJDPlQNGso5bF6tYJDa9aK+XcyWUCgYAAhO1Q0in7vi4YUQofx1kO\nscec4vRHP1z6WL2l6seIEDLXoM2o0+HRZaJgQ+qyWQe/iSSYnqkE/wDj/UjmXkJj\nU3ZgIvkt0mEiPPjyG8kfFLt6Ppj9zwEpTH96q9sonNBOUMMrs3zirwbF2Lal2Z+8\nDfZoWwYsJLnKyuqOh+p3HQ==\n-----END PRIVATE KEY-----\n",
+  mongoURI: "",
+  //   googleProjectID: process.env.GOOGLE_PROJECT_ID,
+  //   dialogFlowSessionID: process.env.DIALOGFLOW_SESSION_ID,
+  //   dialogFlowSessionLanguageCode: process.env.DIALOGFLOW_LANGUAGE_CODE,
+  //   googleClientEmail: process.env.GOOGLE_CLIENT_EMAIL,
+  //   googlePrivateKey: JSON.parse(process.env.GOOGLE_PRIVATE_KEY),
+  //   mongoURI: process.env.MONGO_URI,
 };
